74481a1f2c5a159122fc9ea557910386bde476ef
Aidan Sherry <aidansherry24@gmail.com>
2022-07-28 14:42:18 -0400

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+    House.create(name: "Devin's House", source: "Bawhston", author: "Devin R", motto: "who's house?? run's house.")
+    House.create(name: "Slytherin", source: "Harry Potter", author: "J.K. Rowling", motto: "We like snakes a lot")
+    House.create(name: "Nick's Banana Kingdom", source: "Launch Academy", author: "Nick Alberts")
+    House.create(name: "Gremlin House", source: "That one 80s movie", author: "Gizmo")
